41f4339bb2e8ef4888da7b4e8b3d623e89cef09c
MNoichl <34612138+MNoichl@users.noreply.github.com>
2023-03-04 21:32:14 +0100

    Added __init__.py to data directory

---
+++ opinionated/data/__init__.py
@@ -0,0 +1,15 @@
+"""opinionated - A new Python package"""
+
+import matplotlib as mpl
+from matplotlib import font_manager as fm
+import pkg_resources
+
+# from .core import 
+# __version__ = pkg_resources.require("opinionated")[0].version
+__author__ = 'Maximilian Noichl <noichlmax@hotmail.co.uk>'
+__all__ = []
+
+# register the included stylesheet in the mpl style library
+data_path = pkg_resources.resource_filename('opinionated', 'data/')
+opinionated_stylesheets = mpl.style.core.read_style_directory(data_path)
+mpl.style.core.update_nested_dict(mpl.style.library, opinionated_stylesheets)
