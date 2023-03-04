abb038892dd493ec837d8d81fca7f9cb32e46de5
MNoichl <34612138+MNoichl@users.noreply.github.com>
2023-03-04 22:15:49 +0100

    Added font_download

--- opinionated/core.py
+++ opinionated/core.py
@@ -10,33 +10,13 @@ from pathlib import Path
 
 
 # download fonts from google fonts and save them in the fonts folder:
-def download_font(font='Roboto Condensed'):
+def download_googlefont(font='Roboto Condensed'):
     
     # download the font
     url = f'https://fonts.google.com/download?family={font}'
     r = requests.get(url)
     z = zipfile.ZipFile(io.BytesIO(r.content))
-    z.extractall()
-    
-    # move the font to the fonts folder
-    font_folder = Path(__file__).parent / 'fonts'
-    font_folder.mkdir(exist_ok=True)
-    for f in os.listdir():
-        if f.endswith('.ttf'):
-            shutil.move(f, font_folder / f)
-    
-    # remove the downloaded font folder
-    shutil.rmtree(font_folder / font)
-    
-    # update the font cache
-    fm._rebuild()
-    
-    # print the font path
+    # extract into the package folder
+    font_folder = Path(opinionated.__file__).parent / 'fonts'
+    z.extractall(font_folder)
     print(f'Font saved to: {font_folder / f}')
-
-
-
-
-
-
-
