934f3b5522d2c020d35980ae58ac8e92484e5536
MNoichl <34612138+MNoichl@users.noreply.github.com>
2023-03-25 19:51:57 +0100

    monkypatched legend function in mpl

--- opinionated/__init__.py
+++ opinionated/__init__.py
@@ -6,41 +6,81 @@ import pkg_resources
 from typing import Optional, Union, List, Tuple
 import os
 
-from .core import download_googlefont, show_installed_fonts, update_matplotlib_fonts, add_attribution, add_legend
+from .core import (
+    download_googlefont,
+    show_installed_fonts,
+    update_matplotlib_fonts,
+    add_attribution,
+    add_legend,
+)
 
 
 # __version__ = pkg_resources.require("opinionated")[0].version
-__author__ = 'Maximilian Noichl <noichlmax@hotmail.co.uk>'
+__author__ = "Maximilian Noichl <noichlmax@hotmail.co.uk>"
 __all__ = []
 
 # register the included stylesheet in the mpl style library
-data_path = pkg_resources.resource_filename('opinionated', 'data/')
+data_path = pkg_resources.resource_filename("opinionated", "data/")
 opinionated_stylesheets = mpl.style.core.read_style_directory(data_path)
 mpl.style.core.update_nested_dict(mpl.style.library, opinionated_stylesheets)
 
 # check if the font is already installed WE SHOULD DO THIS....
 
-fonts = ['Roboto Condensed', 'Montserrat', 
-         'Source Code Pro', 'Source Sans Pro',
-         'Fira Sans','Fira Sans Condensed', 'IBM Plex Sans',
-         'Space Grotesk', 'Space Mono',
-         'Roboto','Roboto Condensed', 'Jost']
+fonts = [
+    "Roboto Condensed",
+    "Montserrat",
+    "Source Code Pro",
+    "Source Sans Pro",
+    "Fira Sans",
+    "Fira Sans Condensed",
+    "IBM Plex Sans",
+    "Space Grotesk",
+    "Space Mono",
+    "Roboto",
+    "Roboto Condensed",
+    "Jost",
+]
 
 
 def check_if_font_already_present(font):
-  # check if a file that contains thefont name is already in the fonts folder:
-  try: 
-    for file in [x.lower() for x in os.listdir('fonts')]:
-      if font.replace(' ','').lower() in file:
-        return True
-    return False
-  except:
-    return False
-  
+    # check if a file that contains thefont name is already in the fonts folder:
+    try:
+        for file in [x.lower() for x in os.listdir("fonts")]:
+            if font.replace(" ", "").lower() in file:
+                return True
+        return False
+    except:
+        return False
+
 
 for font in fonts:
-  if not check_if_font_already_present(font):
-    print('Now downloading: ' + font)
-    download_googlefont(font=font)
+    if not check_if_font_already_present(font):
+        print("Now downloading: " + font)
+        download_googlefont(font=font)
 
 update_matplotlib_fonts()
+
+
+
+
+
+# Monkeypatching matplotlib to change the legend font-width:
+import matplotlib.axes
+import matplotlib.pyplot as plt
+def legend_wrapper(*args, **kwargs):
+    # Extract title from kwargs, if provided
+    title = kwargs.pop("title", None)
+
+    # If title exists, make it bold
+    if title:
+        title = "$\\bf{" + title + "}$"
+        kwargs["title"] = title
+
+    # Call the original legend function with the modified title
+    return original_legend_func(*args, **kwargs)
+
+# Save the original legend function for internal use
+original_legend_func = matplotlib.axes.Axes.legend
+
+# Monkey patch the legend function
+matplotlib.axes.Axes.legend = legend_wrapper
