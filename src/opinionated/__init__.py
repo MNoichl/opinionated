1e2fbdfb1f200330827547000ace0033945718e4
Sam Foreman <saforem2@gmail.com>
2023-09-10 01:08:27 -0500

    Update logic for downloading / saving Google Fonts

--- src/opinionated/__init__.py
+++ src/opinionated/__init__.py
@@ -1,154 +1,116 @@
 """
 src/opinionated/__init__.py
 """
-from __future__ import absolute_import, annotations, division, print_function
+# ruff: noqa: F401
+from __future__ import (
+    absolute_import,
+    annotations,
+    division,
+    nested_scopes,
+    print_function
+)
+import logging
 import os
 import time
 
 from pathlib import Path
-import sys
+from typing import Optional
 
 import matplotlib as mpl
-
-# from matplotlib import font_manager as fm
-import pkg_resources
-# from typing import Optional
-# import os
-# import time 
+import matplotlib.pyplot as plt
 import shutil
-# import glob
-# import colormaps as cmaps
-
-
 
 from opinionated.core import (
     FONTS_DIR,
     STYLES_DIR,
+    STYLES,
+    PROJECT_DIR,
+    FONT_NAMES,
     download_googlefont,
     update_matplotlib_fonts,
 )
 
 from opinionated.utils import (
     add_legend,
-    # add_attribution,
+    add_attribution,
     set_title_and_suptitle
 )
 
-__all__ = [
-    'FONTS_DIR',
-    'STYLES_DIR',
-    'download_googlefont',
-    'update_matplotlib_fonts',
-    'add_legend',
-    # 'add_attribution',
-    'set_title_and_suptitle'
-]
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+os.environ['PROJECT_DIR'] = os.path.abspath(PROJECT_DIR)
+
+# __all__ = [
+#     'FONTS_DIR',
+#     'STYLES_DIR',
+#     'PROJECT_DIR',
+#     'download_googlefont',
+#     'update_matplotlib_fonts',
+#     'add_legend',
+#     # 'add_attribution',
+#     'set_title_and_suptitle'
+# ]
 
 
-PYTHON_VERSION = [int(i) for i in sys.version.split(' ')[0].split('.')]
-# HERE = Path(os.path.abspath(__file__)).parent
-# PROJECT_DIR = HERE.parent.parent
-# FONTS_DIR = PROJECT_DIR.joinpath('fonts')
-# FONTS_DIR.mkdir(parents=True, exist_ok=True)
-# if PYTHON_VERSION[0] >=3 and PYTHON_VERSION[1] >= 8:
-#     from importlib.metadata import PackageNotFoundError, version
-# else:
-#     from importlib.metadata import PackageNotFoundError,
-# if sys.version_info[:2] >= (3, 8):
-#     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
-#     from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
-# else:
-#     from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+def reload_styles(outdir: Optional[os.PathLike] = None):
+    outdir = (
+        Path(mpl.get_configdir()).joinpath('stylelib')
+        if outdir is None else Path(outdir)
+    )
+    outdir.mkdir(parents=True, exist_ok=True)
+    for src in STYLES.values():
+        dst = outdir.joinpath(Path(src).stem)
+        log.debug(f"Copying {src} to {dst}")
+        shutil.copy2(src, dst)
 
-# try:
-#     # Change here if project is renamed and does not equal the package name
-#     dist_name = __name__
-#     __version__ = version(dist_name)
-# except PackageNotFoundError:  # pragma: no cover
-#     __version__ = "unknown"
-# finally:
-#     del version, PackageNotFoundError
-
-
-def reload_styles():
-    # register the included stylesheet in the mpl style library
-    # data_path = pkg_resources.resource_filename("opinionated", "data/")
-    # print(data_path)
-    opinionated_stylesheets = mpl.style.core.read_style_directory(STYLES_DIR)
-    mpl.style.core.update_nested_dict(mpl.style.library, opinionated_stylesheets)
-    mpl.style.reload_library()
-    stylefiles = Path(
-        pkg_resources.resource_filename("opinionated", "data/")
-    ).rglob('*.mplstyle')
-    STYLE_FILES = [STYLES_DIR.rglob('*.mplstyle')]
+    plt.style.reload_library()
+    opinionated_stylesheets = plt.style.core.read_style_directory(STYLES_DIR)
+    plt.style.core.update_nested_dict(plt.style.library, opinionated_stylesheets)
+    plt.style.reload_library()
     mpl_stylelib_dir = Path(mpl.get_configdir()).joinpath('stylelib')
     mpl_stylelib_dir.mkdir(parents=True, exist_ok=True)
-    # mpl_stylelib_dir = os.path.join(mpl.get_configdir() ,"stylelib")
-    for stylefile in stylefiles:
-        shutil.copy(
-            stylefile,
-            mpl_stylelib_dir.joinpath(os.path.basename(stylefile))
-            # os.path.join(mpl_stylelib_dir, os.path.basename(stylefile))
-        )
-        
-    # # Update the list of available styles  
-    mpl.pyplot.style.core.available[:] = sorted(mpl.pyplot.style.library.keys())
-    mpl.style.reload_library()
+    # # Update the list of available styles
+    plt.style.core.available[:] = sorted(plt.style.library.keys())
+    # mpl.pyplot.style.core.available[:] = sorted(mpl.pyplot.style.library.keys())
+    plt.style.reload_library()
 
-
-# 
-# check if the font is already installed (WE SHOULD DO THIS)....
-
-FONT_NAMES = {
-    'Fira Sans',
-    'Fira Sans Condensed',
-    'IBM Plex Sans',
-    'Jost',
-    'Montserrat',
-    'Roboto',
-    'Roboto Condensed',
-    'Source Code Pro',
-    # 'Source Sans Pro',
-    'Space Grotesk',
-    'Space Mono',
-    'Titillium Web',
-    # 'Titillium WebRoboto Condensed'
-}
-
-FONT_PATHS = [
-    FONTS_DIR.joinpath(f) for f in FONT_NAMES
-]
 
 def check_if_font_already_present(font):
     return FONTS_DIR.joinpath(font).exists()
 
+
 def download_font_with_retry(font, retries=3, delay=3):
     for i in range(retries):
         try:
-            print(f"Now downloading: {font}")
+            log.debug(f"Now downloading: {font}")
             download_googlefont(font=font)
             return  # return if the download was successful
         except Exception as e:
             if i < retries - 1:  # i is zero indexed
-                print(
+                log.debug(
                     f"Attempt {i+1} to download {font} failed with error:"
                     f"{str(e)}. Retrying in {delay} seconds."
                 )
                 time.sleep(delay)
             else:
-                print(
+                log.debug(
                     f"All attempts to download {font} failed."
                     "Please check your connection and the font name."
                 )
                 raise
 
 
-for font in FONT_NAMES:
-    if FONTS_DIR.joinpath(f"{font}.zip").is_file():
-        print(f"{font} already downloaded, continuing!")
-        continue
-    if not check_if_font_already_present(font):
-        download_font_with_retry(font)
 
-update_matplotlib_fonts()
+def update_fonts():
+    for font in FONT_NAMES:
+        if FONTS_DIR.joinpath(f"{font}.zip").is_file():
+            log.debug(f"{font} already downloaded, continuing!")
+            continue
+        if not check_if_font_already_present(font):
+            download_font_with_retry(font)
+    update_matplotlib_fonts()
+
+
+update_fonts()
 reload_styles()
--- src/opinionated/core.py
+++ src/opinionated/core.py
@@ -5,51 +5,91 @@ from __future__ import absolute_import, annotations, division, print_function
 
 import requests
 import zipfile
-import io
 import os
-# import shutil
+import sys
 from pathlib import Path
-import opinionated # this works?
+import io
+
 import matplotlib as mpl
 
-from typing import Optional
+from typing import Optional, Sequence
 
 import matplotlib.pyplot as plt
 from matplotlib import font_manager as fm
 from IPython.core.display import HTML
 
+PYTHON_VERSION = [int(i) for i in sys.version.split(' ')[0].split('.')]
 HERE = Path(os.path.abspath(__file__)).parent
 PROJECT_DIR = HERE.parent.parent
 
 FONTS_DIR = PROJECT_DIR.joinpath('fonts')
-STYLES_DIR = PROJECT_DIR.joinpath('mplstyles')
+STYLES_DIR = PROJECT_DIR.joinpath('stylefiles')
 
 FONTS_DIR.mkdir(parents=True, exist_ok=True)
 STYLES_DIR.mkdir(parents=True, exist_ok=True)
 
-# download fonts from google fonts and save them in the fonts folder:
+STYLE_FILES = list(STYLES_DIR.rglob('*.mplstyle'))
+STYLES = {f.stem: f.as_posix() for f in STYLE_FILES}
+
+FONT_NAMES = (
+    'Fira Sans',
+    'Fira Sans Condensed',
+    'IBM Plex Sans',
+    'IBM Plex Sans Condensed',
+    'IBM Plex Serif',
+    'Jost',
+    'Montserrat',
+    'Roboto',
+    'Roboto Condensed',
+    'Source Code Pro',
+    'Source Sans Pro',
+    'Space Grotesk',
+    'Space Mono',
+    'Titillium Web',
+    'Titillium WebRoboto Condensed',
+    'Shadows Into Light Two'
+)
+
+FONTS = {
+    f: FONTS_DIR.joinpath(f) for f in FONT_NAMES
+}
+
+
+def set_color_cycle(colors: list[str | Sequence[str | int]]) -> None:
+    plt.rcParams['axes.prop_cycle'] = plt.cycler(
+        'color',
+        list(colors)
+    )
+
+
+def _download_font(font: str) -> requests.Response:
+    return requests.get(
+        f'https://fonts.google.com/download?family={font}'
+    )
+
+
+def save_font(font: str) -> None:
+    response = _download_font(font)
+    z = zipfile.ZipFile(io.BytesIO(response.content))
+    z.extractall(FONTS_DIR)
+    zip_fp = FONTS_DIR.joinpath(f'{font}.zip')
+    with open(zip_fp, 'wb') as f:
+        f.write(response.content)
+    print(f'Font saved to: {FONTS_DIR}/{font}')
+
+
 def download_googlefont(
-    font='IBM Plex Sans',
-    add_to_cache=False
+        font='IBM Plex Sans',
+        add_to_cache=True
 ):
-    """download a font from google fonts and save it in the fonts folder
+    """download a font from Google fonts
 
     Args:
         font (str, optional): The font to download.
-        Must be the name used by googlefonts. Defaults to 'Roboto Condensed'.
+        Must be the name used by googlefonts. Defaults to 'IBM Plex Sans'.
     """
-    # download the font
-    from zipfile import ZipFile
     try:
-        r = requests.get(f'https://fonts.google.com/download?family={font}')
-        z = zipfile.ZipFile(io.BytesIO(r.content))
-        zip_fp = FONTS_DIR.joinpath(f'{font}.zip')
-        with ZipFile(zip_fp, 'w') as zip:
-            # zip.writestr('name_of_file_in_archive', r.content) #str(io.BytesIO(r.content)))
-            zip.writestr('name_of_file_in_archive', r.content)
-        # extract into the package folder
-        z.extractall(FONTS_DIR)
-        print(f'Font saved to: {FONTS_DIR}/{font}')
+        save_font(font)
     except Exception:
         print(f'Failed to download font: {font}, skipping!')
     if add_to_cache:
@@ -66,8 +106,10 @@ def make_html(fontname):
         f"font-size: 24px;'>{fontname}</p>"
     )
 
+
 def show_installed_fonts():
-    """Show all installed fonts in a columnized HTML table. Works in notebooks only.
+    """
+    Show all installed fonts in a columnized HTML table. Works in notebooks only.
     """
     code = (
         "\n".join(
@@ -85,74 +127,61 @@ def update_matplotlib_fonts():
     Useful if you downloaded googlefonts to the fonts folder 
     (with download_googlefont) and want to use them in matplotlib.
     """
-    font_folder = Path(opinionated.__file__).parent / 'fonts'
-    for font_file in fm.findSystemFonts(fontpaths=str(font_folder)):
+    for font_file in fm.findSystemFonts(fontpaths=str(FONTS_DIR)):
         if ('.ttf' in font_file) or ('.otf' in font_file):
             try:
-                fm.fontManager.addfont(os.path.join(font_folder,font_file))
+                fm.fontManager.addfont(
+                    os.path.join(
+                        FONTS_DIR,
+                        font_file
+                    )
+                )
             except Exception:
                 print('This font could not be added: ', font_file)
 
 
 def add_legend(*args, **kwargs):
     fig = plt.gcf()
-    handles, labels = fig.axes[0].get_legend_handles_labels()
-    try:
-        is_scatter = type(handles[0]) == mpl.collections.PathCollection
-    except Exception:
-        is_scatter = False
-    
-    try:
-        is_line_plot = type(handles[0]) == mpl.lines.Line2D
-    except Exception:
-        is_line_plot = False
+    ax = plt.gca()
+    handles, _ = fig.axes[0].get_legend_handles_labels()
+    is_scatter = (type(handles[0]) == mpl.collections.PathCollection)
+    # is_line_plot = (type(handles[0]) == mpl.lines.Line2D)
+    # kwargs |= {"bbox_to_anchor": (1.05, .5)}
+    if "bbox_to_anchor" not in kwargs:
+        kwargs["bbox_to_anchor"] = (1.05, .5)
     if is_scatter:
         if "handltextpad" not in kwargs:
-        # if not "handletextpad" in kwargs:
             kwargs["handletextpad"] = 0.
-        # if not "scatterpoints" in kwargs:
         if 'scatterpoints' not in kwargs:
             kwargs["scatterpoints"] = 1
-        # if not "scatteryoffsets" in kwargs:
         if 'scatteryoffsets' not in kwargs:
             kwargs["scatteryoffsets"] = [0]
-
-    # if not "bbox_to_anchor" in kwargs:
-    if "bbox_to_anchor" not in kwargs:
-        kwargs["bbox_to_anchor"] = (1.24, .5)
-    # if is_line_plot:
-
-    legend = plt.legend(*args, **kwargs)
-    legend.get_title().set_fontweight('bold')
-
+    legend = ax.legend(*args, **kwargs)
+    # legend.get_title().set_fontweight('bold')
     if is_scatter:
         [t.set_va('center_baseline') for t in legend.get_texts()]
-
-
     return legend
 
-    # ax.legend(bbox_to_anchor=(1.2, .5),
-    #             borderaxespad=0.0,
-    #             title="$\\bf{" + title + "}$",
-    #             fancybox=True) 
-# Add args
+
 def add_attribution(
-        attrib = 'Attribution goes here',
-        position = [.9, -0.01]
+        attrib: str,
+        position: Optional[tuple[int, int]] = None,
 ):
-    fig = plt.gcf()
+    # fig = plt.gcf()
+    #, bbox={"facecolor":"orange", "alpha":0.5, "pad":5}
+    loc = (.9, -0.01) if position is None else position
     plt.figtext(
-        position[0],
-        position[1],
+        loc[0],
+        loc[1],
         attrib,
         ha="right",
         fontsize=14
-    )#, bbox={"facecolor":"orange", "alpha":0.5, "pad":5})
+    )
 
 
 def set_title_and_suptitle(
         title_string,
-        sub_title_string,
+        sub_title_string: Optional[str] = None,
         position_title: Optional[list] = None,
         position_sub_title: Optional[list] = None,
 ):
@@ -173,20 +202,16 @@ def set_title_and_suptitle(
     position_sub_title = (
         [.12, .918] if position_sub_title is None else position_sub_title
     )
-    # fig = plt.gcf()
-    plt.figtext(
-        position_title[0],
-        position_title[1],
-        title_string,
-        fontsize=26,
-        fontweight='bold',
-        ha='left'
-    )
+    if sub_title_string is not None:
+        plt.figtext(
+            position_title[0],
+            position_title[1],
+            title_string,
+        )
+    else:
+        sub_title_string = title_string
     plt.figtext(
         position_sub_title[0],
         position_sub_title[1],
         sub_title_string,
-        fontsize=14,
-        fontweight='regular',
-        ha='left'
     )
